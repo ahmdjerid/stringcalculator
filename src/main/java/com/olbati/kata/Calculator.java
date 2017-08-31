f8a6748ef308fdb011f6c21f6eff6c346493c9a9
Ahmed Jerid <ahmed.jerid@olbati.com>
2017-08-31 11:36:21 +0200

    Initial commit

---
+++ src/main/java/com/olbati/kata/Calculator.java
@@ -0,0 +1,13 @@
+package com.olbati.kata;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/olbati/kata/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.olbati.kata;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Calculator.
+ */
+public class CalculatorTest {
+
+}
